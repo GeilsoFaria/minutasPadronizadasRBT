0e2b4bde0273e6196b018131fbdd1ef25514b9a9
Geilso Faria <109383516+GeilsoFaria@users.noreply.github.com>
2024-08-21 15:25:22 -0400

    atualizei justificativa para textarea

---
+++ justificativaAlteracoesContratuais/scripts/script.js
@@ -0,0 +1,19 @@
+
+function receberDados(){
+
+
+    var contrato = document.getElementById('contrato')
+    var processo = document.getElementById('processo');
+    var modalidade = document.getElementById('modalidade');
+    var modalidadeNumero = document.getElementById('modalidadeNumero');
+    var objeto = document.getElementById('objeto');
+    var contratada = document.getElementById('contratada');
+    var cnpj = document.getElementById('cnpj');
+
+    console.log(contrato, processo, modalidade, modalidadeNumero, objeto, contratada, cnpj)
+
+
+
+}
+
+
